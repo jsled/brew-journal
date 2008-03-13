b53d00e3886684ce620027e503afd542a67d5487
Joshua Sled <jsled@phoenix.asynchronous.org>
2008-03-13 10:06:24 -0400

    update static resource path. :p

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns(
     '',
     (r'^admin/', include('django.contrib.admin.urls')),
     # static media
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/jsled/stuff/proj/scratch/brewlog/app/media'}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/jsled/stuff/proj/brewlog/app/media'}),
     (r'^', include('brewlog.app.urls')),
 )
