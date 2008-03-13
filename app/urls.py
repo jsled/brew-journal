ad528a7468b31e926150ce75de42c57d83c7fddf
Joshua Sled <jsled@phoenix.asynchronous.org>
2008-03-13 12:23:35 -0400

    User creation and profiles.

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
     (r'^logout/$', 'logout_view'),
     # @fixme: new-user stuff
     (r'^user/(?P<user_name>[^/]+)/?$', 'user_index'),
+    (r'^user/(?P<user_name>[^/]+)/profile/?$', 'user_profile'),
     (r'^user/(?P<user_name>[^/]+)/brew/(?P<brew_id>\d+)(/step/(?P<step_id>\d+))?/?$', 'brew'),
     (r'^user/(?P<user_name>[^/]+)/brew/new/?$', 'new_brew'),
 )
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseServerError, HttpResponseForbidden, HttpResponseBadRequest
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from genshi.template import TemplateLoader
@@ -36,16 +36,33 @@ class AuthForm (forms.Form):
 
 def root(request):
     auth_form = AuthForm()
+    user = None
     if request.method == 'POST':
         auth_form = AuthForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
         submit_type = request.POST['submit']
         if submit_type == 'create':
-            # @fixme create new user
+            user = None
+            try:
+                user = User.objects.get(username__exact = username)
+            except User.DoesNotExist:
+                pass
+            if user:
+                # error; user already existss
+                return HttpResponseBadRequest('user with username [%s] already exists' % (username))
+            else:
+                user = User.objects.create_user(username, '', password)
+                if not user:
+                    # error creating user
+                    return HttpResponseServerError('unknown error creating user [%s]' % (username))
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/user/%s/profile' % (username))
             pass
         elif submit_type == 'login':
-            # auth.
-            user = authenticate(username = request.POST['username'],
-                                password = request.POST['password'])
+            user = authenticate(username = username,
+                                password = password)
             if not user:
                 # error: username/password incorrect
                 pass
@@ -63,7 +80,6 @@ def root(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
-    
 
 def user_index(request, user_name):
     u = User.objects.get(username__exact = user_name)
@@ -71,18 +87,48 @@ def user_index(request, user_name):
     brews = models.Brew.objects.filter(brewer=u)
     return HttpResponse(render('user/index.html', request=request, user=u, brews=brews))
 
+class UserProfileForm (forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','email')
+    new_pass_1 = forms.CharField(widget=forms.PasswordInput, label="change password", required=False)
+    new_pass_2 = forms.CharField(widget=forms.PasswordInput, label="change password (again)", required=False)
+
+def user_profile(request, user_name):
+    uri_user = User.objects.get(username__exact = user_name)
+    if not uri_user: return HttpResponseNotFound('no such user [%s]' % (user_name))
+    if request.user.is_authenticated() and request.user != uri_user:
+        return HttpResponseForbidden('you [%s] can access the profile for user [%s]' % (request.user.username, uri_user.userrname))
+    profile_form = UserProfileForm(instance=uri_user)
+    if request.method == 'POST':
+        # handle save
+        profile_form = UserProfileForm(request.POST, instance=uri_user)
+        if not profile_form.errors:
+            new_user = profile_form.save(commit=False)
+            uri_user.first_name = new_user.first_name
+            uri_user.last_name = new_user.last_name
+            uri_user.email = new_user.email
+            new_pass_1,new_pass_2 = request.POST['new_pass_1'],request.POST['new_pass_2']
+            if new_pass_1 and new_pass_1 == new_pass_2:
+                uri_user.set_password(new_pass_1)
+            uri_user.save()
+            return HttpResponseRedirect('/user/%s' % (uri_user.username))
+    return HttpResponse(render('user/profile.html', request=request, user=uri_user, profile_form=profile_form, std=standard_context()))
+
 # sys.stdout = codecs.getwriter('utf-8')(sys.stdout, errors='replace')
 def new_brew(request, user_name):
-    u = User.objects.get(username__exact = user_name)
-    if not u: return HttpResponseNotFound('no such user [%s]' % (user_name))
+    uri_user = User.objects.get(username__exact = user_name)
+    if not uri_user: return HttpResponseNotFound('no such user [%s]' % (user_name))
     form_gen = forms.form_for_model(models.Brew)
     form = form_gen()
     if request.method == 'POST':
+        if not (request.user.is_authenticated() and request.user == uri_user):
+            return HttpResponseForbidden('not allowed')
         form = form_gen(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/user/%s' % (u.username))
-    return HttpResponse(render('user/brew/new.html', request=request, user=u, brew_form=form, Markup=Markup))
+            brew = form.save()
+            return HttpResponseRedirect('/user/%s/brew/%s' % (uri_user.username, brew.id))
+    return HttpResponse(render('user/brew/new.html', request=request, user=uri_user, brew_form=form, Markup=Markup))
 
 class StepForm (forms.ModelForm):
     notes = forms.CharField(widget=forms.Textarea())
@@ -92,13 +138,13 @@ class StepForm (forms.ModelForm):
 
 def brew(request, user_name, brew_id, step_id):
     '''
-    /user/jsled/brew/2[/step/3]
+    e.g., /user/jsled/brew/2[/step/3]
     
     @todo time should be a function of the last step time, unless it is outside epsilon of `now`.
-    @todo when editing an existing step, the type (at least) shouldn't be changeable.
-    ''' # emacs '
-    u = User.objects.get(username__exact = user_name)
-    if not u: return HttpResponseNotFound('no such user [%s]' % (user_name))
+    @todo when editing an existing step, the type (at least) should not be changeable.
+    '''
+    uri_user = User.objects.get(username__exact = user_name)
+    if not uri_user: return HttpResponseNotFound('no such user [%s]' % (user_name))
     brew = models.Brew.objects.get(id=brew_id)
     if not brew: return HttpResponseNotFound('no such brew with id [%d]' % (brew_id))
     form = None
@@ -110,6 +156,8 @@ def brew(request, user_name, brew_id, step_id):
         submit_label = 'Update Step'
         print u'step_id [%s] = step [%s]' % (step_id, step)
     if request.method == 'POST':
+        if not (request.user.is_authenticated() and request.user == uri_user):
+            return HttpResponseForbidden()
         form = StepForm(request.POST, instance=step)
         if form.is_valid():
             form.save()
@@ -124,6 +172,5 @@ def brew(request, user_name, brew_id, step_id):
         if len(steps) > 0:
             last_step_type = steps[len(steps)-1].type
             next_step_type = models.get_likely_next_step_type(last_step_type)
-            print u'got next step [%s]' % (next_step_type)
         form = StepForm(initial={'brew': brew.id, 'date': datetime.now(), 'type': next_step_type})
-    return HttpResponse(render('user/brew/index.html', request=request, std=standard_context(), user=u, brew=brew, steps=steps, step_form=form, submit_label=submit_label))
+    return HttpResponse(render('user/brew/index.html', request=request, std=standard_context(), user=uri_user, brew=brew, steps=steps, step_form=form, submit_label=submit_label))
