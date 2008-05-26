2a0a18586b9ebad58d685c736cb29ac3e6c2b7cc
Joshua Sled <jsled@phoenix.(none)>
2008-05-26 16:07:09 -0400

    notes on adding recipe star-ing

--- app/urls.py
+++ app/urls.py
@@ -12,6 +12,10 @@ urlpatterns = patterns(
     (r'^user/(?P<user_name>[^/]+)/brew/(?P<brew_id>\d+)/edit/$', 'brew_edit'),
 
     (r'^user/(?P<user_name>[^/]+)/recipe/$', 'user_recipe_index'),
+
+    # GET /user/jsled/star?recipe_id=42 + POST
+    # GET /user/jsled/star?url=<http://.../recipe> + POST
+    # (r'^user/(?P<user_name>[^/]+)/recipe/star/$', 'user_recipe_star'),
     
     # /recipe/new/?clone_from_recipe_id=42
     (r'^recipe/new/$', 'recipe_new'),
