e3698ebddddff08dbbf16c73ac113a352c13b16b
Joshua Sled <jsled@phoenix.(none)>
2008-06-14 10:51:02 -0400

    Revert "local settings" This reverts commit 6dfbca458fcc2c646800af3be8850c689d8a47cc.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Django settings for brew-journal project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns(
     '',
     (r'^admin/', include('django.contrib.admin.urls')),
     # static media
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/jsled/brewjournal/app/media'}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/jsled/stuff/proj/brewjournal/app/media'}),
     (r'^', include('brewjournal.app.urls')),
 )
