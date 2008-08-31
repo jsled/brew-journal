fcc74e2e5f0e244196d4cabd72b0aa82dec2d525
Joshua Sled <jsled@phoenix.(none)>
2008-08-31 17:57:46 -0400

    some test case faffery

--- app/tests.py
+++ app/tests.py
@@ -3,8 +3,11 @@ import unittest
 from django.test.client import Client
 from django.test import TestCase
 
-class BasicLoginTest (TestCase):
-    fixtures = ['auth', 'grains1', 'grains2', 'hops', 'yeast-manufacturers', 'yeasts', 'styles']
+class AppTestCase (TestCase):
+    fixtures = ['auth', 'grains1', 'grains2', 'grains3', 'hops', 'hops1', 'yeasts1', 'adjuncts', 'yeast-manufacturers', 'yeasts', 'styles']
+
+
+class BasicLoginTest (AppTestCase):
 
     def testFailed(self):
         res = self.client.post('/', {'username': 'bob', 'password': '', 'sub': 'login'})
@@ -23,3 +26,25 @@ class NotLoggedInMeansNoActions (TestCase):
         res = self.client.get('/user/jsled/')
         # self.assertNotContains(res, u'»', 200)
         self.assert_(res.content.find('»') == -1)
+
+
+class HopWithoutBoilTime (AppTestCase):
+    def test(self):
+        app = self.client
+        app.login(username='jsled', password='s3kr1t')
+        res = app.post('/recipe/new/', {'name': 'test', 'insert_date': '2008-08-31 20:48', 'batch_size': 5, 'batch_size_units': 'gl', 'style': 1, 'type': 'a'})
+        new_recipe_url = res['Location']
+        base_recipe_url = '/'.join(new_recipe_url.split('/')[3:-1])
+        hop_post_url = '/%s/hop/' % (base_recipe_url)
+        res = app.post(hop_post_url, {'hop': 1, 'amount_value': 2, 'amount_units': 'oz'})
+        # @fixme:
+        #assert(res.body.contains validation error mumble on boil-time field)
+        self.assertEqual(302, res.status_code)
+
+# - creating a new user
+#   - invalid user name
+# - creating a recipe
+#   - adding items to a recipe
+#   - changing a recipe
+# - creating a brew from a recipe
+#   - adding steps to the brew
